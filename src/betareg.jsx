0220bacadfe0e47ed785191029ec157bd28db6c8
GENIE <0xhenery@gmail.com>
2025-01-02 10:59:23 +0630

    Update betareg.jsx

--- src/betareg.jsx
+++ src/betareg.jsx
@@ -11,7 +11,7 @@ const Wlform = () => {
 
   const handleSubmit = async(e) => {
     e.preventDefault();
-    await fetch(`https://genieai-gamma.vercel.app/`,{
+    await fetch(`https://genieai-backend.vercel.app/`,{
       method: "POST",
       body:JSON.stringify(data),
       headers:{
